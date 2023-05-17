604a4f0fc98f31c21c5de1620d5b75b61c2442a7
Hari <haririo321@gmail.com>
2023-05-17 18:45:44 +0530

    Lil changes here and there

--- new_flutter_app/lib/main.dart
+++ new_flutter_app/lib/main.dart
@@ -1,19 +1,22 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'My Flutter App',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Welcome to Flutter'),
+          backgroundColor: Colors.black,
+          title: const Text('Welcome to Flutter'),
         ),
-        body: Center(
+        body: const Center(
           child: Text('Hello, Flutter!'),
         ),
       ),
