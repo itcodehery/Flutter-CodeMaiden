4f8b1e20b2e81f15a22277865ffe85d493d80c01
Hari <haririo321@gmail.com>
2023-05-20 20:14:51 +0530

    Music Dictionary: App

--- musidict/lib/main.dart
+++ musidict/lib/main.dart
@@ -2,28 +2,43 @@ import 'package:flutter/material.dart';
 void main(){
   runApp(const MyApp());
 }
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  IconData iconstate = Icons.add;
+  @override
   Widget build(BuildContext context) {
+    
     return MaterialApp(
       home: Scaffold(appBar: AppBar(
         backgroundColor: Colors.deepOrange,
         elevation: 0,
         title: const Text('Musidict'),
       ),
-      body: Container(
-        alignment: Alignment.centerLeft,
-        margin: const EdgeInsets.all(10),
-        padding: const EdgeInsets.all(10),
-        color: Colors.teal,
-        height: 100,
-        width: 100,
-        decoration: BoxDecoration(
-          border: Border.all(color: const Color.fromARGB(255, 170, 33, 33), width: 1.0) 
+      floatingActionButton: FloatingActionButton(
+        child: Icon(iconstate),
+        onPressed: () {
+          iconstate == Icons.add ? iconstate = Icons.check : iconstate = Icons.add;
+        },
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
           ),
-          child: const Text('Hi Mom! 🤩') 
+          BottomNavigationBarItem(icon: Icon(Icons.music_note), label: 'Your Music'),
+          BottomNavigationBarItem(icon: Icon(Icons.settings), label: 'Settings'),
+          BottomNavigationBarItem(icon: Icon(Icons.account_circle_rounded), label: 'Account')
+        ]
+      ),
+      drawer: const Drawer(
+        child: Text('Yo!!'), 
         ),
       )
     );
