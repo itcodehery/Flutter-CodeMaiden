fd780e09af962b27b6cba0cbb3940df01195934d
Hari <haririo321@gmail.com>
2023-06-08 17:10:41 +0530

    Revert "Cleaned up the UI. Calculator is done."
    This reverts commit 69e08b5546ed26d0378d83262b8b5e4350655734.

--- calculator/lib/main.dart
+++ calculator/lib/main.dart
@@ -1,16 +1,19 @@
+<<<<<<< HEAD
 import 'dart:math';
+=======
+>>>>>>> parent of 69e08b5 (Cleaned up the UI. Calculator is done.)
 import 'package:flutter/material.dart';
 
-//try keeping the code clean this time.
 void main() => runApp(const CalculatorApp());
 
 class CalculatorApp extends StatelessWidget {
   const CalculatorApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Two Operand Calculator',
-      theme: ThemeData(primarySwatch: Colors.orange),
+      title: 'Calculator',
+      theme: ThemeData(primarySwatch: Colors.blue),
       home: const CalculatorScreen(),
     );
   }
@@ -41,9 +44,7 @@ class CalculatorScreenState extends State<CalculatorScreen> {
       } else if (buttonText == '+' ||
           buttonText == '-' ||
           buttonText == '*' ||
-          buttonText == '/' ||
-          buttonText == 'M' ||
-          buttonText == '^') {
+          buttonText == '/') {
         _num1 = double.parse(_output);
         _operator = buttonText;
         _output = '0';
@@ -68,15 +69,6 @@ class CalculatorScreenState extends State<CalculatorScreen> {
 
           case '/':
             _result = (_num1 / _num2).toString();
-            break;
-
-          case 'M':
-            _result = (_num1 % _num2).toString();
-            break;
-
-          case '^':
-            _result = pow(_num1, _num2).toString();
-            break;
         }
         _num1 = 0.0;
         _num2 = 0.0;
@@ -98,11 +90,6 @@ class CalculatorScreenState extends State<CalculatorScreen> {
         onPressed: () => _buttonPressed(buttonText),
         style: ButtonStyle(
           backgroundColor: MaterialStateProperty.all(buttonColor),
-          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-            RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(12.0),
-                side: const BorderSide(color: Colors.transparent)),
-          ),
         ),
         child: Text(
           buttonText,
@@ -112,31 +99,6 @@ class CalculatorScreenState extends State<CalculatorScreen> {
             color: Colors.white,
           ),
         ));
-  }
-
-  Widget _buildEqualsButton(
-      String buttonText, Color buttonColor, double buttonHeight) {
-    return SizedBox(
-      width: 130,
-      child: TextButton(
-          onPressed: () => _buttonPressed(buttonText),
-          style: ButtonStyle(
-            backgroundColor: MaterialStateProperty.all(buttonColor),
-            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-              RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(12.0),
-                  side: const BorderSide(color: Colors.transparent)),
-            ),
-          ),
-          child: Text(
-            buttonText,
-            style: const TextStyle(
-              fontSize: 30.0,
-              fontWeight: FontWeight.normal,
-              color: Colors.black,
-            ),
-          )),
-    );
   }
 
   @override
@@ -159,104 +121,42 @@ class CalculatorScreenState extends State<CalculatorScreen> {
             child: Divider(), //Expands the divider when numbers are big
           ),
           Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               Row(
-                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  _buildButton('7', Colors.pinkAccent, 1),
-                  const SizedBox(
-                    width: 6,
-                  ),
-                  _buildButton('8', Colors.pinkAccent, 1),
-                  const SizedBox(
-                    width: 6,
-                  ),
-                  _buildButton('9', Colors.pinkAccent, 1),
-                  const SizedBox(
-                    width: 6,
-                  ),
+                  _buildButton('7', Colors.grey, 1),
+                  _buildButton('8', Colors.grey, 1),
+                  _buildButton('9', Colors.grey, 1),
                   _buildButton('/', Colors.orange, 1),
-                  const SizedBox(
-                    width: 6,
-                  ),
-                  _buildButton('.', Colors.orange, 1),
+                  _buildButton('.', Colors.grey, 1),
                 ],
               ),
-              const SizedBox(
-                height: 6,
-              ),
               Row(
-                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  _buildButton('4', Colors.pinkAccent, 1),
-                  const SizedBox(
-                    width: 6,
-                  ),
-                  _buildButton('5', Colors.pinkAccent, 1),
-                  const SizedBox(
-                    width: 6,
-                  ),
-                  _buildButton('6', Colors.pinkAccent, 1),
-                  const SizedBox(
-                    width: 6,
-                  ),
+                  _buildButton('4', Colors.grey, 1),
+                  _buildButton('5', Colors.grey, 1),
+                  _buildButton('6', Colors.grey, 1),
                   _buildButton('*', Colors.orange, 1),
-                  const SizedBox(
-                    width: 6,
-                  ),
-                  _buildButton('M', Colors.orange, 1),
                 ],
               ),
-              const SizedBox(
-                height: 6,
-              ),
               Row(
-                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  _buildButton('1', Colors.pinkAccent, 1),
-                  const SizedBox(
-                    width: 6,
-                  ),
-                  _buildButton('2', Colors.pinkAccent, 1),
-                  const SizedBox(
-                    width: 6,
-                  ),
-                  _buildButton('3', Colors.pinkAccent, 1),
-                  const SizedBox(
-                    width: 6,
-                  ),
+                  _buildButton('1', Colors.grey, 1),
+                  _buildButton('2', Colors.grey, 1),
+                  _buildButton('3', Colors.grey, 1),
                   _buildButton('-', Colors.orange, 1),
-                  const SizedBox(
-                    width: 6,
-                  ),
-                  _buildButton('^', Colors.orange, 1)
                 ],
               ),
-              const SizedBox(
-                height: 6,
-              ),
               Row(
-                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  _buildButton('C', Colors.pinkAccent, 1),
-                  const SizedBox(
-                    width: 6,
-                  ),
-                  _buildButton('0', Colors.pinkAccent, 1),
-                  const SizedBox(
-                    width: 6,
-                  ),
-                  _buildEqualsButton('=', Colors.orangeAccent, 1),
-                  const SizedBox(
-                    width: 6,
-                  ),
+                  _buildButton('C', Colors.grey, 1),
+                  _buildButton('0', Colors.grey, 1),
+                  _buildButton('=', Colors.orange, 1),
                   _buildButton('+', Colors.orange, 1),
                 ],
               ),
             ],
           ),
-          const Padding(padding: EdgeInsets.all(6)),
         ],
       ),
     );
