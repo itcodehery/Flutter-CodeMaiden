87b47b2ef18642fee4dd5a07796abf6637309876
Hari <haririo321@gmail.com>
2023-06-08 13:06:30 +0530

    Added new Calculator App

---
+++ calculator/lib/main.dart
@@ -0,0 +1,149 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const CalculatorApp());
+
+class CalculatorApp extends StatelessWidget {
+  const CalculatorApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Calculator',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const CalculatorScreen(),
+    );
+  }
+}
+
+class CalculatorScreen extends StatefulWidget {
+  const CalculatorScreen({super.key});
+
+  @override
+  CalculatorScreenState createState() => CalculatorScreenState();
+}
+
+class CalculatorScreenState extends State<CalculatorScreen> {
+  String _output = '0';
+  double _num1 = 0.0;
+  double _num2 = 0.0;
+  String _operator = '';
+  String _result = '';
+
+  void _buttonPressed(String buttonText) {
+    setState(() {
+      if (buttonText == 'C') {
+        _output = '0';
+        _num1 = 0.0;
+        _num2 = 0.0;
+        _operator = '';
+        _result = '';
+      } else if (buttonText == '+' ||
+          buttonText == '-' ||
+          buttonText == '*' ||
+          buttonText == '/') {
+        _num1 = double.parse(_output);
+        _operator = buttonText;
+        _output = '0';
+      } else if (buttonText == '=') {
+        _num2 = double.parse(_output);
+        if (_operator == '+') {
+          _result = (_num1 + _num2).toString();
+        }
+        if (_operator == '-') {
+          _result = (_num1 - _num2).toString();
+        }
+        if (_operator == '*') {
+          _result = (_num1 * _num2).toString();
+        }
+        if (_operator == '/') {
+          _result = (_num1 / _num2).toString();
+        }
+        _num1 = 0.0;
+        _num2 = 0.0;
+        _operator = '';
+        _output = _result;
+      } else {
+        if (_output == '0') {
+          _output = buttonText;
+        } else {
+          _output += buttonText;
+        }
+      }
+    });
+  }
+
+  Widget _buildButton(
+      String buttonText, Color buttonColor, double buttonHeight) {
+    return ElevatedButton(
+      onPressed: () => _buttonPressed(buttonText),
+      child: Text(
+        buttonText,
+        style: const TextStyle(
+          fontSize: 30.0,
+          fontWeight: FontWeight.normal,
+          color: Colors.white,
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Calculator')),
+      body: Column(
+        children: [
+          Container(
+            alignment: Alignment.centerRight,
+            padding:
+                const EdgeInsets.symmetric(horizontal: 12.0, vertical: 24.0),
+            child: Text(
+              _output,
+              style:
+                  const TextStyle(fontSize: 48.0, fontWeight: FontWeight.bold),
+            ),
+          ),
+          const Expanded(
+            child: Divider(),
+          ),
+          Column(
+            children: [
+              Row(
+                children: [
+                  _buildButton('7', Colors.grey, 1),
+                  _buildButton('8', Colors.grey, 1),
+                  _buildButton('9', Colors.grey, 1),
+                  _buildButton('/', Colors.orange, 1),
+                ],
+              ),
+              Row(
+                children: [
+                  _buildButton('4', Colors.grey, 1),
+                  _buildButton('5', Colors.grey, 1),
+                  _buildButton('6', Colors.grey, 1),
+                  _buildButton('*', Colors.orange, 1),
+                ],
+              ),
+              Row(
+                children: [
+                  _buildButton('1', Colors.grey, 1),
+                  _buildButton('2', Colors.grey, 1),
+                  _buildButton('3', Colors.grey, 1),
+                  _buildButton('-', Colors.orange, 1),
+                ],
+              ),
+              Row(
+                children: [
+                  _buildButton('C', Colors.grey, 1),
+                  _buildButton('0', Colors.grey, 1),
+                  _buildButton('=', Colors.orange, 1),
+                  _buildButton('+', Colors.orange, 1),
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
