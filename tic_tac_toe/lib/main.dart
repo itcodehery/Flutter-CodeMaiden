0a62696d353310f8047d8f4e9323268f953707e1
Hari Prasad <haririo321@gmail.com>
2023-07-08 11:33:21 +0530

    Working state

--- tic_tac_toe/lib/main.dart
+++ tic_tac_toe/lib/main.dart
@@ -36,58 +36,46 @@ class TTTHomeScreenState extends State<TTTHomeScreen> {
           title: const Text("TicTacToe"),
           centerTitle: true,
         ),
-        body: Column(
-          children: [
-            Padding(
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(8),
+            child: GridView.builder(
               padding: const EdgeInsets.all(8),
-              child: GridView.builder(
-                padding: const EdgeInsets.all(8),
-                itemCount: 9,
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 3),
-                itemBuilder: (BuildContext context, int index) {
-                  return GestureDetector(
-                    onTap: () {
-                      setState(() {
-                        switch (index) {
-                          case 0:
-                            _gridVariable = 'X';
-                            break;
-                          case 1:
-                            _gridVariable = 'Y';
-                            break;
-                          case 2:
-                            _gridVariable = 'Z';
-                            break;
-                        }
-                      });
-                    },
-                    child: Container(
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                            color: Colors.white, style: BorderStyle.solid),
-                      ),
-                      child: Center(
-                        child: Text(_gridVariable,
-                            style: const TextStyle(
-                                fontSize: 40, color: Colors.white)),
-                      ),
+              itemCount: 9,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3),
+              itemBuilder: (BuildContext context, int index) {
+                return GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      switch (index) {
+                        case 0:
+                          _gridVariable = 'X';
+                          break;
+                        case 1:
+                          _gridVariable = 'Y';
+                          break;
+                        case 2:
+                          _gridVariable = 'Z';
+                          break;
+                      }
+                    });
+                  },
+                  child: Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                          color: Colors.white, style: BorderStyle.solid),
                     ),
-                  );
-                },
-              ),
+                    child: Center(
+                      child: Text(_gridVariable,
+                          style: const TextStyle(
+                              fontSize: 40, color: Colors.white)),
+                    ),
+                  ),
+                );
+              },
             ),
-            const SizedBox(
-              height: 20,
-            ),
-            const Text(
-              "You won the game!",
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 28,
-              ),
-            )
-          ],
+          ),
         ));
   }
 }
